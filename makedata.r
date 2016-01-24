051649c678d5b5483b7825bd0920d3dcc219c12c
akui <sekainoishi@gmail.com>
2016-01-25 03:26:37 +0800

    读数据

--- makedata.r
+++ makedata.r
@@ -1,7 +1,7 @@
 library(stringr)
 
 
-format <- function( word ){
+kaki <- function( word ){
     answer = str_sub(str_extract(word,"\\（.*\\）"), 2,-2)#提取答案
     quiz = sub(pattern="\\（.*）",replacement="x",word) #把答案替换成x
     monji = gsub(pattern="(\\［[^］]*］)|(｛[^｝]*｝)",replacement=" ",quiz) #去掉注音
@@ -13,20 +13,36 @@ format <- function( word ){
     return( out )
 }
           
+yomi <- function( word ){
+    answer = str_sub(str_extract(word,"\\（.*\\）"), 2,-2)#提取答案
+    kanji = str_sub(str_extract(word,"\\｛.*\\｝"), 2,-2)#提取答案汉字做索引
+    quiz = sub(pattern="\\（.*）",replacement="（x）",word) #把答案替换成（x）
+    #去掉注音
+    monji = gsub(pattern="(\\［[^］]*］)|（x）",replacement=" ",quiz) 
+    monji = gsub(pattern="[\\｛\\｝]",replacement="",monji) 
+    monji = str_trim(monji, side = "both" )
+    
+    kana = str_c(str_sub( str_extract_all(quiz,"（x）|\\［[^\\］]*\\］")[[1]],
+                          2, -2 ), collapse = " ")
+    
+    out = c(monji, kana, answer, kanji)
+    return( out )
+}
+
 file = "C:/Users/LENOVO/OneDrive/kanji/data/"
 for (i in 10:11) {
-    name = paste(file, "my_kaki_", i, ".txt", sep="")
+    name = paste(file, "my_yomi_", i, ".txt", sep="")
     print(name)
     
     data=readLines(name,encoding ="UTF-8" )        
     #每一行为汉字部分,注音部分,答案
-    barabara = t( sapply( data, format,USE.NAMES = F ) )
-    kanjis = levels( factor( barabara[,3] ) )
+    barabara = t( sapply( data, yomi,USE.NAMES = F ) )
+    kanjis = levels( factor( barabara[,4] ) )
     #每一个汉字所在行数
-    zu = t( sapply(kanjis, function(x)  which(barabara[, 3] == x) ) ) 
-    write.table(zu, paste("index", i, ".csv", sep=""), 
+    zu = t( sapply(kanjis, function(x)  which(barabara[, 4] == x) ) ) 
+    write.table(zu, paste("kakiindex", i, ".csv", sep=""), 
                 col.names = F, sep=",")
-    write.table(barabara,paste("quiz", i, ".csv", sep=""), 
+    write.table(barabara,paste("kakiquiz", i, ".csv", sep=""), 
                 col.names = F, row.names = F, sep=",")
 }
 
